91e3dbf099a79f275e0e57d61280a5347d5276d9
Md Noorullah Raza <mnraza94@gmail.com>
2025-09-08 16:13:20 +0530

    Refactor server startup logic and update package.json for clarity

--- server/app.js
+++ server/app.js
@@ -1,14 +1,25 @@
 import express from "express";
 import userRoutes from "./routes/user.js";
 import dotenv from "dotenv";
-
 dotenv.config();
-
 const app = express();
 app.use(express.json());
+const start = async () => {
+  try {
+    app.listen(process.env.PORT, "0.0.0.0", (err, addr) => {
+
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log(`Server is running on port http://localhost:${process.env.PORT}`);
+      }
+    });
+  } catch (error) {
+    console.log("Error starting the server: ", error);
+  }
+}
 
 app.use("/api/user", userRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port http://localhost:${process.env.PORT}`);
-});
+
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,9 @@ import express from "express";
 import { loginOrRegister } from "../controllers/user.js";
 const router = express.Router();
 
+router.get("/get-data", (req, res) => {
+    res.status(200).json({ message: "Get user data route is working" });
+});
 router.post("/login", loginOrRegister);
 
 export default router;
